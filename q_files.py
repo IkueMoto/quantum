508c7ef019e9e313c2d8132084c2e7d7f479cedf
Andrew Chou <a.chou@f5.com>
2023-09-20 11:19:49 +0100

    copy files from incoming to working directory

---
+++ q_files.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+def to_working(extension=".tar.gz"):
+    source_folder = os.getcwd()  # Get the current working directory
+    destination_folder = source_folder.replace("incoming", "working")
+    # Check if the destination folder exists, and create it if not
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    # Iterate over files in the source folder
+    for filename in os.listdir(source_folder):
+        source_path = os.path.join(source_folder, filename)
+        if filename.endswith(extension):
+            # Check if the file is a .tar.gz file
+            destination_path = os.path.join(destination_folder, filename)
+            shutil.copyfile(source_path, destination_path)
+            print(f"Copied {filename} to {destination_folder}")
+
+# Example usage: Copy all .tar.gz (by default) files from current incoming directory to the working directory.
+# Takes an extension eg .txt as an optional argument
+if __name__ == '__main__':
+  to_working()
+  to_working(".txt")
